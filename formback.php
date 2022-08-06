791c1a8d036876a644ff92a8c45f45dd84752a69
ChularatB <91866956+ChularatB@users.noreply.github.com>
2022-08-06 12:37:13 +0700

    Add files via upload

---
+++ formback.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta name="author" content="Banchar Paseelatesang">
+	<meta name="description" content="Echo Form Data">
+	<meta charset="utf-8">
+	<title>form back</title>
+</head>
+
+<body>
+
+	<h1>...เข้าสู่ระบบแล้ว...</h1>
+	<form action="Lap2.3.html">
+		<input type="submit" name="logout" value="Logout">
+	</form>
+
+
+	<?php
+	{}
+	?>
+
+</body>
+
+</html>
